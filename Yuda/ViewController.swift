1c35607491c3f862a8bff3d1ca192cf87bc2ab17
yusuShin <tlsdbtn0507@naver.com>
2024-11-30 23:27:09 +0900

    init

--- Yuda/ViewController.swift
+++ Yuda/ViewController.swift
@@ -11,9 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("ViewController: Loaded!")
+
+        // 배경색 설정
+        view.backgroundColor = .white
+
+        // 간단한 UILabel 추가
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(label)
+
+        // Auto Layout 설정
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
-
