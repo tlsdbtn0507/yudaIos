f1909b96953d0f205084fa1a9fd30c1e0986cc22
yusuShin <tlsdbtn0507@naver.com>
2024-11-30 23:09:38 +0900

    Initial Commit

---
+++ Yuda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yuda
+//
+//  Created by 신유수 on 11/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YudaTests/YudaTests.swift
@@ -0,0 +1,17 @@
+//
+//  YudaTests.swift
+//  YudaTests
+//
+//  Created by 신유수 on 11/30/24.
+//
+
+import Testing
+@testable import Yuda
+
+struct YudaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
