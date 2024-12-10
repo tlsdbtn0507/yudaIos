37e7b22d3bce3640805ec6d89b8fa38c40b1dd15
yusuShin <tlsdbtn0507@naver.com>
2024-12-10 20:09:46 +0900

    feat ios내에서도 alert 적용 / 인풋 클릭시 하단에 보기 안좋은 done버튼 영역 삭제

--- Yuda/ViewController.swift
+++ Yuda/ViewController.swift
@@ -7,13 +7,33 @@
 import UIKit
 import WebKit
 
+extension WKWebView {
+    private static var hasSwizzled = false
+
+    static func swizzleInputAccessoryView() {
+        guard !hasSwizzled else { return }
+
+        hasSwizzled = true
+
+        let selector = NSSelectorFromString("inputAccessoryView")
+        guard let originalMethod = class_getInstanceMethod(WKWebView.self, selector) else { return }
+
+        let block: @convention(block) (Any?) -> UIView? = { _ in return nil }
+        let newImplementation = imp_implementationWithBlock(block)
+
+        method_setImplementation(originalMethod, newImplementation)
+    }
+}
+
 class ViewController: UIViewController, WKScriptMessageHandler {
     
     var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // WKWebView의 inputAccessoryView 제거를 위한 Swizzling 호출
+        WKWebView.swizzleInputAccessoryView()
+
         // JavaScript 메시지 핸들러 설정
         let contentController = WKUserContentController()
         contentController.add(self, name: "nativeAlert") // 핸들러 이름 등록
@@ -21,6 +41,8 @@ class ViewController: UIViewController, WKScriptMessageHandler {
         let config = WKWebViewConfiguration()
         config.userContentController = contentController
         webView = WKWebView(frame: .zero, configuration: config)
+        
+        
         view.addSubview(webView)
         
         // Auto Layout 설정
@@ -40,10 +62,11 @@ class ViewController: UIViewController, WKScriptMessageHandler {
     // WKScriptMessageHandler 필수 구현
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if message.name == "nativeAlert", let messageBody = message.body as? String {
-            // UIAlertController로 메시지 표시
-            let alert = UIAlertController(title: "알림", message: messageBody, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            DispatchQueue.main.async { // UI 작업을 메인 스레드에서 실행
+                let alert = UIAlertController(title: "알림", message: messageBody, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
         }
     }
 }
